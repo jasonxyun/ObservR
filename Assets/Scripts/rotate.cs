4d1964f817bf89c43d98f2f669eddc8becdfc052
Jason <jasonyun123@gmail.com>
2017-12-04 17:13:01 -0800

    first commit

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class rotate : MonoBehaviour {
+	public float speed = 250f;
+	public bool isRotating = false;
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.F))
+			transform.Rotate (Vector3.up * speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.H))
+			transform.Rotate (-Vector3.up * speed * Time.deltaTime);
+		if (Input.GetKeyDown (KeyCode.Y)) {
+			transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
+		}
+		if (Input.GetKey (KeyCode.T)) {
+			transform.Rotate(-Vector3.right * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.G)) {
+			transform.Rotate(Vector3.right * speed * Time.deltaTime);
+		}
+	}
+
+}
